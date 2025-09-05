584eda426a02bb0e8f31b3990e6c42086ec7972a
nagma <bano.nagma.1@gmail.com>
2025-09-05 20:52:23 +0530

    component composition: children prop

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,24 +1,36 @@
 import "./App.css";
-import { CORE_CONCEPTS } from  './data.js'; // named exports are imported with curly braces
+import { CORE_CONCEPTS } from "./data.js"; // named exports are imported with curly braces
 import Header from "./components/Header/Header.jsx";
 import CoreConcept from "./components/CoreConcepts.jsx";
+import TabButton from "./components/TabButton.jsx";
 
 function App() {
   return (
     <div>
-      <Header></Header> 
+      <Header></Header>
       <main>
         <section id="core-concepts">
-        <h2>Core Concepts</h2>
-        <ul>
-          <CoreConcept 
-            title={CORE_CONCEPTS[0].title} 
-            description={CORE_CONCEPTS[0].description}
-            image={CORE_CONCEPTS[0].image} />
-          <CoreConcept {...CORE_CONCEPTS[1]} />{/* JS spread operator */}
-          <CoreConcept {...CORE_CONCEPTS[2]} />
-          <CoreConcept {...CORE_CONCEPTS[3]} />
-        </ul>
+          <h2>Core Concepts</h2>
+          <ul>
+            <CoreConcept
+              title={CORE_CONCEPTS[0].title}
+              description={CORE_CONCEPTS[0].description}
+              image={CORE_CONCEPTS[0].image}
+            />
+            <CoreConcept {...CORE_CONCEPTS[1]} />
+            {/* JS spread operator */}
+            <CoreConcept {...CORE_CONCEPTS[2]} />
+            <CoreConcept {...CORE_CONCEPTS[3]} />
+          </ul>
+        </section>
+        <section id="examples">
+          <h2>Examples</h2>
+          <menu>
+            <TabButton>Components</TabButton>
+            <TabButton>JSX</TabButton>
+            <TabButton>Props</TabButton>
+            <TabButton>State</TabButton>
+          </menu>
         </section>
       </main>
     </div>
