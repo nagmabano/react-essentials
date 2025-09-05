36ddac152a4f8da642c901ca89230c548c069829
nagma <bano.nagma.1@gmail.com>
2025-09-05 19:31:50 +0530

    restructuring the code in seperate files

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,35 +1,7 @@
 import "./App.css";
-import reactImage from './assets/react-core-concepts.png';
 import { CORE_CONCEPTS } from  './data.js'; // named exports are imported with curly braces
-
-const reactDescriptions = ["Fundamental", "Crucial", "Core"];
-
-function genRandomInt(max) {
-  return Math.floor(Math.random() * (max+1));
-}
-
-function Header() {
-  const description = reactDescriptions[genRandomInt(2)];
-  return (
-    <header>
-      <img src={reactImage} alt="Stylized atom" />
-      <h1>React Essentials</h1>
-      <p>{ description } react concepts you will need for almost any app you are going
-      to build! 
-      </p>
-    </header>
-  );
-}
-
-function CoreConcept({image, title, description}) { // JS destructuring
-  return (
-    <li>
-      <img src={image} alt={title}/>
-      <h3>{title}</h3>
-      <p>{description}</p>
-    </li>
-  );
-}
+import Header from "./components/Header.jsx";
+import CoreConcept from "./components/CoreConcepts.jsx";
 
 function App() {
   return (
