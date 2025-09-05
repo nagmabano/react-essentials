4ca15ead343b827516cb23d96961af5d9777f40e
nagma <bano.nagma.1@gmail.com>
2025-09-05 13:36:43 +0530

    created custom Header component

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,22 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function Header() {
+  return (
+    <header>
+      <img src="src/assets/react.svg" alt="stylized atom" />
+      <h1>React Essentials</h1>
+      <p></p>
+      Fundamental react concepts you will need for almost any app you are going
+      to build!
+    </header>
+  );
+}
 
 function App() {
   return (
     <div>
-      <header>
-        <img src='src/assets/react.svg' alt='stylized atom'/>
-        <h1>React Essentials</h1>
-        <p>Fundamental react concepts you will need for almost any app you are going to build!</p>
-      </header>
-
+      <Header></Header> 
       <main>
         <h2>Time to get started!</h2>
       </main>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
