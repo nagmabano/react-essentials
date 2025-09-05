75ca6d5746b1fcd9aeae365b795ff534e83ee392
nagma <bano.nagma.1@gmail.com>
2025-09-05 16:03:55 +0530

    using dynamic values

--- react-project/src/App.jsx
+++ react-project/src/App.jsx
@@ -1,13 +1,20 @@
 import "./App.css";
 
+const reactDescriptions = ["Fundamental", "Crucial", "Core"];
+
+function genRandomInt(max) {
+  return Math.floor(Math.random() * (max+1));
+}
+
 function Header() {
+  const description = reactDescriptions[genRandomInt(2)];
   return (
     <header>
       <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
       <h1>React Essentials</h1>
-      <p></p>
-      Fundamental react concepts you will need for almost any app you are going
-      to build!
+      <p>{ description } react concepts you will need for almost any app you are going
+      to build! 
+      </p>
     </header>
   );
 }
